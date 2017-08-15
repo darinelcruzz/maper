520422b7f367e6b6e33ef2324c44f5635cbc3e6d
Victor <victorjcg_6@hotmail.com>
2017-08-14 23:00:23 -0500

    no se

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -89,55 +89,5 @@
             </tr>
         </template>
 
-        <template slot="body">
-            @foreach($paid as $row)
-              <tr>
-                  <td><a href="{{ route('service.general.details', ['id' => $row->id]) }}"> {{ $row->id }} </a></td>
-                  <td>{{ $row->short_date_out }}</td>
-                  <td>{{ $row->inventory }}</td>
-                  <td>{{ $row->service }}</td>
-                  <td>{{ $row->brand }} - {{ $row->type }} - {{ $row->color }}</td>
-                  <td>${{ $row->amount + $row->maneuver + $row->pension - $row->discount }}</td>
-                  <td>
-                      <a href="{{ route('service.corporation.edit', ['id' => $row->id]) }}" class="btn btn-info">
-                          <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                      </a>
-                  </td>
-              </tr>
-            @endforeach
-        </template>
-    </data-table>
-    <data-table col="col-md-12" title="Liberados" example="example4" color="box-success" collapsed="collapsed-box">
-        <template slot="header">
-            <tr>
-                <th>ID</th>
-                <th>Fecha Liberación</th>
-                <th>Inventario</th>
-                <th>Tipo</th>
-                <th>Marca</th>
-                <th>Liberador</th>
-                <th>Importe</th>
-                <th>Opciones</th>
-            </tr>
-        </template>
 
-        <template slot="body">
-            @foreach($release as $row)
-              <tr>
-                  <td><a href="{{ route('service.corporation.details', ['id' => $row->id]) }}"> {{ $row->id }} </a></td>
-                  <td>{{ $row->short_date_out }}</td>
-                  <td>{{ $row->inventory }}</td>
-                  <td>{{ $row->service }}</td>
-                  <td>{{ $row->brand }} - {{ $row->type }} - {{ $row->color }}</td>
-                  <td>{{ $row->releaser }}</td>
-                  <td>${{ $row->amount + $row->maneuver + $row->pension - $row->discount }}</td>
-                  <td>
-                      <a href="{{ route('service.corporation.edit', ['id' => $row->id]) }}" class="btn btn-info">
-                          <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
-                      </a>
-                  </td>
-              </tr>
-            @endforeach
-        </template>
-    </data-table>
 @endsection
