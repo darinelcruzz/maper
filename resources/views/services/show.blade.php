cce04aba9e7c604694fed92123d7d892b365b772
Victor <victorjcg_6@hotmail.com>
2017-08-14 23:07:46 -0500

    client name in show

--- resources/views/services/show.blade.php
+++ resources/views/services/show.blade.php
@@ -2,7 +2,43 @@
 
 @section('main-content')
 
+    <data-table col="col-md-12" title="General" example="example1" color="box-primary" collapsed="collapsed-box">
+        <template slot="header">
+            <tr>
+                <th>ID</th>
+                <th>Fecha</th>
+                <th>Cliente</th>
+                <th>Vehículo</th>
+                <th>Operador</th>
+                <th>Estimado</th>
+                <th>Opciones</th>
+            </tr>
+        </template>
 
+        <template slot="body">
+            @foreach($general as $row)
+              <tr>
+                  <td><a href="{{ route('service.general.details', ['id' => $row->id]) }}"> {{ $row->id }} </a></td>
+                  <td>{{ $row->short_date }}</td>
+                  <td>{{ $row->clientr->name }}</td>
+                  <td>{{ $row->brand }} - {{ $row->type }} - {{ $row->color }}</td>
+                  <td>{{ $row->driverr->name }}</td>
+                  <td>${{ $row->amount }}</td>
+                  <td>
+                      <a href="{{ route('service.general.pay', ['id' => $row->id]) }}" class="btn btn-success">
+                          <i class="fa fa-check" aria-hidden="true"></i>
+                      </a>
+                      <a href="#" class="btn btn-danger">
+                          <i class="fa fa-times" aria-hidden="true"></i>
+                      </a>
+                      <a href="{{ route('service.general.edit', ['id' => $row->id]) }}"  class="btn btn-info">
+                          <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                      </a>
+                  </td>
+              </tr>
+            @endforeach
+        </template>
+    </data-table>
 
     <data-table col="col-md-12" title="Corporaciones" example="example2" color="box-primary" collapsed="collapsed-box">
         <template slot="header">
@@ -46,7 +82,7 @@
                 <th>ID</th>
                 <th>Fecha Pago</th>
                 <th>Inventario</th>
-                <th>Client</th>
+                <th>Cliente</th>
                 <th>Marca</th>
                 <th>Importe</th>
                 <th>Opciones</th>
@@ -59,7 +95,7 @@
                   <td><a href="{{ route('service.general.details', ['id' => $row->id]) }}"> {{ $row->id }} </a></td>
                   <td>{{ $row->short_date_out }}</td>
                   <td>{{ $row->inventory }}</td>
-                  <td>{{ $row->service }}</td>
+                  <td>{{ $row->clientr->name }}</td>
                   <td>{{ $row->brand }} - {{ $row->type }} - {{ $row->color }}</td>
                   <td>${{ $row->amount + $row->maneuver + $row->pension - $row->discount }}</td>
                   <td>
