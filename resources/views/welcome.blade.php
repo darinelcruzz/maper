62559bfdf12d18c581065fdfad0402a649010c17
Darinel Cruz <darinelcruzz@gmail.com>
2023-03-06 00:31:13 -0600

    Welcome view updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @if($days >= 8)
     <div align="center" valign="middle">
         <br><br><br><br><br><br>
-        <h1>Mas de {{ $days}} sin hacer corte</h1>
+        <h1>Mas de {{ floor($days) }} día(s) sin hacer corte</h1>
         <h1>último corte el {{ fdate($cut, 'D d/m/Y', 'Y-m-d') }}</h1>
     </div>
 @else
