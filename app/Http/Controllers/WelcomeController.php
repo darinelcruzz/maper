83c5b76f71d4dc2a99a5d4f73d34cfa61c2dcf22
Victor Cancino <victorjcg_6@hotmail.com>
2023-09-05 13:48:57 -0600

    Welcome controller changed, II

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,13 +10,13 @@ class WelcomeController extends Controller
 {
     function index()
     {
-        $service = Service::where('cut_at', '!=', 'null')
-            ->orderBy('id', 'desc')
+        $service = Service::whereNotNull('cut_at')
+            ->latest()
             ->first();
 
         // dd($service);
 
-        $cut = $service->cut_at ?? date('Y-m-d');
+        $cut = $service->cut_at;
         $days = (time() - strtotime($cut))/86400;
 
         return view('welcome', compact('cut', 'days'));
